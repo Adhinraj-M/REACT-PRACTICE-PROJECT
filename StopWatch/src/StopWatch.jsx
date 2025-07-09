a92063cbe9fbc4fe18b477cd23e53d9b50c2288a
Adhin Raj <adhinrajcr7@gmail.com>
2025-07-09 11:50:07 +0530

    weather app

--- StopWatch/src/StopWatch.jsx
+++ StopWatch/src/StopWatch.jsx
@@ -65,3 +65,8 @@ function StopWatch() {
 }
 
 export default StopWatch;
+
+
+
+
+
---
+++ WeatherApp/src/Component/NoLocation.jsx
@@ -0,0 +1,8 @@
+
+function NoLocation() {
+  return (
+    <div className="relative flex justify-center text-red-800 text-4xl font-semibold">No Matching Location Found !</div>
+  )
+}
+
+export default NoLocation
